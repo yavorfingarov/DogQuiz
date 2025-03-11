7d7e4199b3ce4c18826208a4ea313f4795dd9813
Yavor Fingarov <yavor.fingarov@outlook.com>
2025-03-11 10:58:36 +0200

    Added more unit tests

--- tests/DogQuiz.UnitTests/Common/ConfigurationTests.cs
+++ tests/DogQuiz.UnitTests/Common/ConfigurationTests.cs
@@ -3,76 +3,24 @@
     public sealed class ConfigurationTests
     {
         [Fact]
-        public Task DogApiBaseUrl()
+        public Task Snapshot()
         {
-            var attributes = GetAttributes(nameof(Configuration.DogApiBaseUrl));
+            var configuration = new Dictionary<string, object?>();
+            var properties = typeof(Configuration).GetProperties();
+            foreach (var property in properties)
+            {
+                var getMethod = property.GetGetMethod();
+                if (getMethod != null && getMethod.IsStatic)
+                {
+                    configuration[property.Name] = getMethod.Invoke(null, Array.Empty<object?>());
+                }
+                else
+                {
+                    configuration[property.Name] = property.GetCustomAttributes();
+                }
+            }
 
-            return Verify(attributes);
-        }
-
-        [Fact]
-        public Task ImagesPath()
-        {
-            var attributes = GetAttributes(nameof(Configuration.ImagesPath));
-
-            return Verify(attributes);
-        }
-
-        [Fact]
-        public Task ImageTargetSize()
-        {
-            var attributes = GetAttributes(nameof(Configuration.ImageTargetSize));
-
-            return Verify(attributes);
-        }
-
-        [Fact]
-        public Task ImageTargetQuality()
-        {
-            var attributes = GetAttributes(nameof(Configuration.ImageTargetQuality));
-
-            return Verify(attributes);
-        }
-
-        [Fact]
-        public Task ImageTargetMinFileSize()
-        {
-            var attributes = GetAttributes(nameof(Configuration.ImageTargetMinFileSize));
-
-            return Verify(attributes);
-        }
-
-        [Fact]
-        public Task QuizLength()
-        {
-            var attributes = GetAttributes(nameof(Configuration.QuizLength));
-
-            return Verify(attributes);
-        }
-
-        [Fact]
-        public Task QuizExpirationInDays()
-        {
-            var attributes = GetAttributes(nameof(Configuration.QuizExpirationInDays));
-
-            return Verify(attributes);
-        }
-
-        [Fact]
-        public Task CleanerWaitHours()
-        {
-            var attributes = GetAttributes(nameof(Configuration.CleanerWaitHours));
-
-            return Verify(attributes);
-        }
-
-        private static IEnumerable<Attribute> GetAttributes(string propertyName)
-        {
-            var attributes = typeof(Configuration)
-                .GetProperty(propertyName)!
-                .GetCustomAttributes();
-
-            return attributes;
+            return Verify(configuration);
         }
     }
 }
